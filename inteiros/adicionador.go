2a90db280e437212438a79b3f16008a68bcd0d40
GiovanePS <giovanespace@gmail.com>
2024-02-18 22:27:59 -0300

    adding a example in adicionador

--- inteiros/adicionador.go
+++ inteiros/adicionador.go
@@ -1,5 +1,6 @@
 package inteiros
 
+// Adiciona recebe dois inteiros e retorna a soma deles
 func Adiciona(n, m int) int {
 	return n + m
 }
--- inteiros/adicionador_test.go
+++ inteiros/adicionador_test.go
@@ -1,6 +1,9 @@
 package inteiros
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestAdicionador(t *testing.T) {
 	soma := Adiciona(2, 2)
@@ -9,4 +12,10 @@ func TestAdicionador(t *testing.T) {
 	if soma != expected {
 		t.Errorf("output: %d, expected: %d", soma, expected)
 	}
+}
+
+func ExampleAdiciona() {
+	soma := Adiciona(1, 5)
+	fmt.Println(soma)
+	// Output: 6
 }
