8bdda521caeda6242fde5afbf98bdd32aeb17ba5
GiovanePS <giovanespace@gmail.com>
2024-02-25 19:10:39 -0300

    finishing concorrencia by adding it and tests for that

--- concorrencia/verificaWebsites.go
+++ concorrencia/verificaWebsites.go
@@ -1,12 +1,24 @@
 package concorrencia
 
 type VerificadorWebsite func(string) bool
+type result struct {
+	string
+	bool
+}
 
 func VerificaWebsites(vw VerificadorWebsite, urls []string) map[string]bool {
 	results := make(map[string]bool)
+	resultChannel := make(chan result)
 
 	for _, url := range urls {
-		results[url] = vw(url)
+		go func(u string) {
+			resultChannel <- result{u, vw(u)}
+		}(url)
+	}
+
+	for i := 0; i < len(urls); i++ {
+		result := <-resultChannel
+		results[result.string] = result.bool
 	}
 
 	return results
